e65ab62352859c864ac024b2a2103713dd4670fa
Gerardo Cella <gerardocella7@gmail.com>
2020-07-24 12:23:17 +0200

    Ajax

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,7 +1,23 @@
 (() => {
   const quote = document.getElementById('quote');
   const btnRefresh = document.getElementById('refresh');
-
+  let httpRequest = null;
+  if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+...
+    httpRequest = new XMLHttpRequest();
+  }
+  else if (window.ActiveXObject) { // IE 6 et antérieurs
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  httpRequest.onreadystatechange = () => {
+    // instructions de traitement de la réponse
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        view(JSON.parse(httpRequest.responseText));
+      } else {
+        error('Il y a eu un problème avec la requête.');
+      }
+    }
+  };
 
   const view = (data) => {
     const template = document.getElementById('tpl-quote');
@@ -30,6 +46,16 @@
   }
 
   const refresh = async () => {
+    if (!httpRequest) {
+      alert('Abandon :( Impossible de créer une instance de XMLHTTP');
+      return false;
+    }
+    httpRequest.open('GET', 'https://thatsthespir.it/api', true);
+    httpRequest.send();
+
+
+
+
     try {
       await fetch('https://thatsthespir.it/api')
         .then(res => res.json())
@@ -38,6 +64,7 @@
       error(e);
     }
   };
+
   btnRefresh.addEventListener('click', refresh);
   refresh();
 })();
